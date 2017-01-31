aba8bfd143250d6bc7f55fc03e3f2c67c7d6c37f
dragosh <dragosh@Air.local>
2017-01-31 19:35:55 +0200

    connecting to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+var dbPath = process.env.MONGODB_URI || "mongodb://localhost/app1";
+var db;
 
-app.get('/', (req, res) => res.send('Up and running!'));
+mongoose.connect(dbPath, (err, database) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Connected to mongodb');
 
-var port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`running on port : ${port}`)); 
+    app.get('/', (req, res) => res.send('Up and running!'));
+
+    var port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`running on port : ${port}`));
+
+});
